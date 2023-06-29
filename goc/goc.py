92b603ebd85e7dac047dac68fdb652f854f49c96
Todd Perry <perry@nowcast.co.jp>
2023-06-29 13:10:56 +0100

    rework to pass args directly into git diff

--- goc/goc.py
+++ goc/goc.py
@@ -18,17 +18,9 @@ def document_latest_commit():
     ]
     return prompt_chain
 
-def document_comparison_vs_current_commit(benchmark):
-    cmd = f"git diff {benchmark}"
-    git_diff = exec_bash_cmd(cmd)
-    prompt_chain = [
-        'I send you a git diff, and you write documentation of the commit in markdown',
-        git_diff
-    ]
-    return prompt_chain
-
-def document_comparison_of_2_commits(commit_a, commit_b):
-    cmd = f"git diff {commit_a, commit_b}"
+def document_comparison_vs_current_commit(args):
+    fmt_args = " ".join(args)
+    cmd = f"git diff {fmt_args}"
     git_diff = exec_bash_cmd(cmd)
     prompt_chain = [
         'I send you a git diff, and you write documentation of the commit in markdown',
@@ -47,17 +39,17 @@ def execute_prompt_chain(prompt_chain):
     return resp
 
 def parse_gpt_resp(resp):
-    md_output = resp['choices'][0]['message']['content']
+    md_output = resp['choices'][-1]['message']['content']
     return md_output
 
 def goc():
     n_args = len(sys.argv) - 1
     if n_args == 0:
+        # compare the latest 2 commits
         prompt_chain = document_latest_commit()
-    elif n_args == 1:
-        prompt_chain = document_comparison_vs_current_commit(sys.argv[1])
-    elif n_args == 2:
-        prompt_chain = document_comparison_of_2_commits(sys.argv[1], sys.argv[2])
+    elif n_args > 0:
+        # just pass the args directly into git diff
+        prompt_chain = document_git_diff_wrap(sys.argv[1:])
     resp = execute_prompt_chain(prompt_chain)
     md_output = parse_gpt_resp(resp)
     print(md_output)

1d2bc163abd2449187c90040c50ac38f664dd739
Todd Perry <perry@nowcast.co.jp>
2023-06-30 16:05:26 +0100

    Added test for executing bash command with multiple lines output

--- tests/test_goc.py
+++ tests/test_goc.py
@@ -15,6 +15,17 @@ class ExecBashCmdTests(unittest.TestCase):
         mock_popen.assert_called_with(cmd)
         mock_popen.return_value.read.assert_called_once()
 
+    @patch("os.popen")
+    def test_exec_bash_cmd_multiple_lines_output(self, mock_popen):
+        mock_popen.return_value.read.return_value = "line1\nline2\nline3\n"
+        cmd = "some_command"
+
+        result = target.exec_bash_cmd(cmd)
+
+        self.assertEqual(result, "line1\nline2\nline3")
+        mock_popen.assert_called_with(cmd)
+        mock_popen.return_value.read.assert_called_once()
+
 if __name__ == "__main__":
     unittest.main()
 
